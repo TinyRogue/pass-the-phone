6c6f722aa7262a7da640c8903e4cbb05aa8f35ba
Wiktor Kania <roller47@wp.pl>
2022-11-11 18:36:46 +0100

    add broken countdown

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(10000);
+  const [startTime, setStartTime] = useState(Date.now());
+  
+  useEffect(() => {
+    setStartTime(Date.now());
+
+    function frameFunc() {
+      const elapsed = Date.now() - startTime;
+      let newCounter = counter - elapsed;
+      if (newCounter < 0) {
+        newCounter = 0;
+        cancelAnimationFrame(frameFunc);
+      }
+      setCounter(newCounter);
+    }
+
+    requestAnimationFrame(frameFunc);
+  }, [counter]);
+  
+
+
   return (
     <View style={styles.container}>
-      <Text>Hello moto</Text>
+      <Text>Time left: {(counter / 1000).toFixed(3)}</Text>
+      <Button title='Next' onPress={() => {
+        setStartTime(Date.now());
+        setCounter(10000);
+      }}></Button>
     </View>
   );
 }
