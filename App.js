86d1ab69977a94ee631bbc3174da5ede2848420d
Wiktor Kania <roller47@wp.pl>
2022-11-16 22:39:04 +0100

    fix resetting counter

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [counter, setCounter] = useState(10000);
   const [startTime, setStartTime] = useState(Date.now());
+
+  const requestRef = useRef();
+
+  function frameFunc() {
+    const elapsed = Date.now() - startTime;
+    const newCounter = counter - elapsed;
+    if (newCounter < 0) {
+      setCounter(0);
+      return cancelAnimationFrame(requestRef.current);
+    }
+    setCounter(newCounter);
+    requestRef.current = requestAnimationFrame(frameFunc);
+  }
   
   useEffect(() => {
-    setStartTime(Date.now());
-
-    function frameFunc() {
-      const elapsed = Date.now() - startTime;
-      let newCounter = counter - elapsed;
-      if (newCounter < 0) {
-        newCounter = 0;
-        cancelAnimationFrame(frameFunc);
-      }
-      setCounter(newCounter);
-    }
-
-    requestAnimationFrame(frameFunc);
-  }, [counter]);
+    requestRef.current = requestAnimationFrame(frameFunc);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [startTime]);
   
 
 
@@ -27,8 +29,8 @@ export default function App() {
     <View style={styles.container}>
       <Text>Time left: {(counter / 1000).toFixed(3)}</Text>
       <Button title='Next' onPress={() => {
-        setStartTime(Date.now());
         setCounter(10000);
+        setStartTime(Date.now());
       }}></Button>
     </View>
   );
